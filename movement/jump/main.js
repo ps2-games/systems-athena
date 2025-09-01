41744641577d339488b0036d278bc90ba32626f7
GibranKhalil <gibrankhalil150904@gmail.com>
2025-09-01 18:06:02 -0300

    feat: add a 2D jump system

---
+++ movement/jump/main.js
@@ -0,0 +1,76 @@
+const GROUND_POSITION = 250;
+const JUMP_STRENGTH = 8;
+const GRAVITY = 0.5;
+const MAX_Y_VELOCITY = 10;
+
+const idle = new Image('./assets/warrior.png')
+idle.width = 96;
+idle.height = 96;
+idle.startx = 0;
+idle.endx = 96;
+idle.starty = 0;
+idle.endy = 96;
+idle.totalFrames = 6;
+idle.frameWidth = 96;
+idle.frameHeight = 96;
+idle.position = {
+    x: 250,
+    y: GROUND_POSITION
+}
+idle.velocity = {
+    x: 0,
+    y: 0
+}
+
+const jump = () => {
+    if (idle.position.y >= GROUND_POSITION) {
+        idle.velocity.y = JUMP_STRENGTH * -1;
+    }
+}
+
+const updatePosition = () => {
+    idle.position = {
+        x: idle.position.x + idle.velocity.x,
+        y: idle.position.y + idle.velocity.y
+    }
+}
+
+const applyGravity = () => {
+    if (idle.position.y < GROUND_POSITION) {
+        idle.velocity.y += GRAVITY;
+    }
+}
+
+const resetVelocityWhenGrounded = () => {
+    if (idle.position.y >= GROUND_POSITION) {
+        idle.position.y = GROUND_POSITION;
+        idle.velocity.y = 0;
+    }
+}
+
+const blockVelocity = () => {
+    if (idle.velocity.y > MAX_Y_VELOCITY) {
+        idle.velocity.y = MAX_Y_VELOCITY;
+    }
+}
+
+const pads = Pads.get();
+
+Screen.display(() => {
+    pads.update();
+
+    if (pads.justPressed(Pads.CROSS)) {
+        jump();
+    }
+
+    applyGravity();
+
+    blockVelocity();
+
+    updatePosition();
+
+    resetVelocityWhenGrounded();
+
+
+    idle.draw(idle.position.x, idle.position.y);
+})
